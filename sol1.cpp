2a2c05d5cbcd0a9df92d1165736566822eb36417
ayush raj <ayushraj.bit17@gmail.com>
2021-09-08 09:27:53 +0530

    sol1.cpp

---
+++ sol1.cpp
@@ -0,0 +1,92 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+ string dup = "";
+ vector<vector<string>> ans;
+
+
+bool isPalindrome(string s)
+{
+    int n = s.size();
+     int i =0,j = n-1;
+      
+       while(i<=j)
+       {
+           if(s[i] != s[j])return false;
+           dup += s[i];
+           i++;
+           j--;
+           
+       }
+       return true;
+}
+
+
+  void solve(int i , string s,vector<string> temp)
+    {
+        if(i >= s.size()) {
+           
+            int k = temp.size();
+            for(auto i : temp){
+                cout << i << " ";
+            }
+
+
+            ans.push_back(temp);
+            return;
+        }
+        
+        
+        for(int j =i;j<s.size();j++)
+        {
+            string left = s.substr(i,j-i + 1);
+            if(isPalindrome(left))
+            {
+                temp.push_back(left);
+                solve(j + 1,s,temp);
+                temp.pop_back();
+            }
+        }
+        
+        
+    }    
+    
+
+
+
+int main() {
+
+int t;
+cin >> t;
+while(t--)
+{
+
+dup = "";
+  
+   string s;
+   cin >> s;
+   int n = s.size();
+   if(isPalindrome(s))
+   {
+      
+       cout << dup << "\n";
+    
+   }
+   else
+   {
+       vector<string> temp;
+        solve(0,s,temp);
+        
+        cout << "\n";
+
+   }
+
+}
+
+	return 0;
+}
+
+
